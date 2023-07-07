84af4d5935d9588f3aaa4f0ddd56c7e0da4a6f71
Soham Agarwal <121bm0729@nitrkl.ac.in>
2023-07-07 15:35:44 +0530

    integrated alan ai

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-
 
 export default function App() {
   useEffect(() => {
     alanBtn({
-      key: '50d057b75dddf67318672098068a95052e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: (commandData) => {
-        if (commandData.command === 'testCommand') {
-          // Call the client code that will react to the received command
-          alert('The command is received')
+      key: "50d057b75dddf67318672098068a95052e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({command, articles}) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
         }
-      }
+      },
     });
   }, []);
 
   return (
     <div>
-      <h1>
-        Alan AI Project
-      </h1>
+      <h1>Alan AI Project</h1>
     </div>
-  )
+  );
 }
